dd53134482af94fbac97254069e1ed07279bd062
Yehor-Sh <153819260+Yehor-Sh@users.noreply.github.com>
2025-09-25 23:59:12 +0200

    Delete backtest directory

--- backtest/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Backtesting utilities for the MyMarket project."""
-
-from .engine import Backtester, BacktestTrade, load_klines_from_csv
-from .metrics import summarize_performance
-
-__all__ = ["Backtester", "BacktestTrade", "load_klines_from_csv", "summarize_performance"]
